64fd9bdcb8ae80526fbe7de6f62ddf8ef4c265db
millykerry <kerryom975@gmail.com>
2025-09-17 01:06:59 +0300

    Fix functions to pass tests for scuberGreetingForFeet and switchOnCharmFromTip
    scuberGreetingForFeet: return 'No can do.' for rides over 2500 feet
    
    switchOnCharmFromTip: added tip parameter and proper switch cases

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  // if the ride 400 feet or less,its free!
+  if (feet <= 400) {
+    return 'This one is on me!';
+}
+  // if the ride is between 400 and 2000 feet, the fare is $2.00
+  else if (feet > 400 && feet <= 2000){
+    return 'That will be twenty bucks.';
+  
+  }
+  // if the ride is between 2000 and 2500 feet, the fare is $25.00
+  else if(feet<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  // if the ride is over 2500 feet, the ride is not allowed
+  if (feet > 2500){
+    return 'No can do.';
+  }
+
+  }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
